9c760d3970710476a2340f0631b3f7f1c22ef8e6
VoidChacal <92901966+VoidChacal@users.noreply.github.com>
2022-10-19 10:49:05 -0300

    Update Exemplo.sql

--- Exemplo.sql
+++ Exemplo.sql
@@ -26,3 +26,25 @@ END $$
 DELIMITER ;
 
 CALL DelPedido(1);
+
+/* Exercicio */
+DELIMITER $$
+CREATE PROCEDURE pedido (IN qt INT, IN vendedor INT, IN cliente INT, IN produto INT)
+BEGIN
+	INSERT INTO pedido (id_vendedor, id_cliente, id_produto, qtd) VALUES ( vendedor, cliente, produto, qtd);
+    SET @idPedido = LAST_INSERT_ID();
+    SET @vl = 0;
+    UPDATE produto SET qtd = qtd-qt WHERE cd = produto;
+    SELECT valor INTO @vl FROM produto WHERE cd = produto;
+    SET @res = (@vl/100)*5;
+    INSERT INTO comissao (id_pedido, id_vendedor, valor) VALUES ( @idPedido, vendedor, @res);
+END $$ 
+DELIMITER ;
+
+CALL pedido(2, 1, 1, 3);
+
+SELECT * FROM pedido;
+SELECT * FROM comissao;
+SELECT * FROM cliente;
+SELECT * FROM produto;
+SELECT * FROM vendedor;
